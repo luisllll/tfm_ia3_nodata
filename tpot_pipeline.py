6cab67618f249fe174f60a5752ddb330a4860891
luisllll <luislloretllinares@gmail.com>
2024-06-09 18:51:25 +0200

    ultima prediccion

--- tpot_pipeline.py
+++ tpot_pipeline.py
@@ -2,9 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
 from tpot.export_utils import set_param_recursive
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,10 +15,20 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.7086956521739131
+# Average CV score on the training set was: 0.7250000000000001
 exported_pipeline = make_pipeline(
-    MinMaxScaler(),
-    RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.2, min_samples_leaf=2, min_samples_split=11, n_estimators=100)
+    make_union(
+        FunctionTransformer(copy),
+        make_union(
+            FunctionTransformer(copy),
+            make_union(
+                FunctionTransformer(copy),
+                FunctionTransformer(copy)
+            )
+        )
+    ),
+    StackingEstimator(estimator=MLPClassifier(activation="relu", alpha=0.001, hidden_layer_sizes=(100, 100), learning_rate="adaptive", solver="adam")),
+    RandomForestClassifier(criterion="gini", max_features="log2", n_estimators=100)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
