33dcaf0b87de7fb5d5fb3199e321786c7df87bd7
luisllll <luislloretllinares@gmail.com>
2024-06-09 12:47:01 +0200

    VAMOS A WUEDARNOS CON TPOT PARA ENTRNEAR EL MODELO DE CLASIFIACIÓN

---
+++ tpot_pipeline.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
+from tpot.export_utils import set_param_recursive
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=42)
+
+# Average CV score on the training set was: 0.7166666666666667
+exported_pipeline = make_pipeline(
+    make_union(
+        make_union(
+            make_union(
+                FunctionTransformer(copy),
+                FunctionTransformer(copy)
+            ),
+            FunctionTransformer(copy)
+        ),
+        StackingEstimator(estimator=MLPClassifier(activation="tanh", alpha=0.001, hidden_layer_sizes=(100,), learning_rate="constant", solver="sgd"))
+    ),
+    RandomForestClassifier(criterion="gini", max_features="log2", n_estimators=100)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
